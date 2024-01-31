321ce235f7f485fe954d777ff03ee40930329476
wolneberg <ingrid_marie@wolneberg.no>
2024-01-31 17:59:20 +0100

    La til pandas i requirements, fjerna unødvendig linje i mobilenet

--- Models/MobileNet_v1/model.py
+++ Models/MobileNet_v1/model.py
@@ -6,9 +6,6 @@ from tensorflow.keras.applications import imagenet_utils
 
 
 mobile = tf.keras.applications.mobilenet.MobileNet()
-base_model = tf.keras.applications.MobileNet(input_shape=IMG_SHAPE,
-                                               include_top=False,
-                                               weights='imagenet')
 
 def prepare_image(file):
     img_path = 'Models/MobileNet_v1/data/MobileNet-samples/'
